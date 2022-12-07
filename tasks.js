a1cbaf30e92d84c58e7363d66355fe39f88cedc3
Gavmat <gavrmat@yandex.ru>
2022-12-07 19:40:45 +0300

    new files

--- tasks.js
+++ tasks.js
@@ -1,12 +1,26 @@
 // Напишите функцию JavaScript, которая переворачивает слово.  
 
 
+// function reverseString(text) {
+//     let newText = "";
+//     for (let i = text.length - 1; i >= 0; i--) {
+//         newText = newText + text[i];
+//     }
+//     return newText;
+// }
+
+// console.log(reverseString('Matvey'));
+
 function reverseString(text) {
-    let newText = "";
-    for (let i = text.length - 1; i >= 0; i--) {
-        newText = newText + text[i];
+
+    const arr = [...text]
+    let reverse = "";
+
+    while (arr.length) {
+        reverse = reverse + arr.pop()
     }
-    return newText;
+
+    return reverse;
 }
 
 console.log(reverseString('Matvey'));
